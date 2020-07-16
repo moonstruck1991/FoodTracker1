4a712e998d6447f54d092c17daea9ca517f01b06
Kanav Pruthi <kanav.pruthi@gmail.com>
2020-07-16 20:37:09 +0530

    made header footer files

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require("express")
-var app 	= express()
-var bodyParser=require('body-parser');
-var https=require('https');
+const express = require("express");
+var app 	= express();
+const bodyParser=require('body-parser');
+const https=require('https');
 const request = require('request');
-const path = require("path")
-var mongoose = require("mongoose")
+const path = require("path");
+const mongoose = require("mongoose");
+
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -22,30 +24,33 @@ var userSchema = new mongoose.Schema({
 		Fat: String,
 		Water: String
 	}]
-})
+});
 
-app.get("/",function(req,res){
+app.route('/')
+	.get(function(req,res){
+		res.render("landing.ejs")
+		
+	})
 	
-	res.render("landing.ejs")
+	.post(function(req,res){
+		var url = "https://api.nutritionix.com/v1_1/search/" + req.body.food+"?fields=item_name,brand_name,item_id,nf_calories&appId=8b2f16ba&appKey=bf903319558c8e7bb3ab4db7687c868d"
+		request(url,function(err, response, body){
+			var parsedData = JSON.parse(body)
+			res.redirect("/dashboard",{data: parsedData})
+		})})
 
-	
-})
+
+
 
 app.get("/dashboard",function(req,res){
 	res.render("dashboard.ejs")
-})
+});
 
 app.get("/details",function(req,res){
 	res.render("details.ejs")
-})
+});
 
-app.post("/",function(req,res){
-	var url = "https://api.nutritionix.com/v1_1/search/" + req.body.food+"?fields=item_name,brand_name,item_id,nf_calories&appId=8b2f16ba&appKey=bf903319558c8e7bb3ab4db7687c868d"
-	request(url,function(err, response, body){
-		var parsedData = JSON.parse(body)
-		res.redirect("/dashboard",{data: parsedData})
-	})})
 
-app.listen(process.env.PORT, process.env.IP, function(){
-	console.log("Food tracker server has started")
+app.listen(process.env.PORT||3000, process.env.IP, function(){
+	console.log("Food tracker server up and running on port 3000");
 })
